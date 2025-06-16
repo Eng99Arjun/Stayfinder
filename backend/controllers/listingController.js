46b73ebac310735ac329546cf851f62517330232
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-06-16 12:16:46 +0530

    cmpltd listing controller

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -74,9 +74,18 @@ const getListings = async (req, res) => {
                 return res.status(401).json({ message: 'Not authorised' });
              }
 
-             Object.assign(listing)
+             Object.assign(listing, req.body);
+             const updatedListing = await listing.save();
+             res.json(updateListing);
 
         } catch (error) {
-            
+            res.status(400).json({ message: 'Invalid listing data' })
         }
     }
+
+    module.exports = { 
+        getListings, 
+        getListingById, 
+        createListing, 
+        updateListing 
+    };
